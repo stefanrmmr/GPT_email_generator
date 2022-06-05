26682e9f010ce218920122e132ac968f69c497b9
Stefan Rummer <82606558+stefanrmmr@users.noreply.github.com>
2022-06-05 06:27:20 +0200

    add space

--- streamlit_app.py
+++ streamlit_app.py
@@ -109,7 +109,7 @@ def main_gpt3emailgen():
                 email_text = gen_mail_format(input_sender, input_recipient, input_contents)
 
     if email_text != "":
-        st.subheader('\nYou will sound incredibly professional with this email!\n')
+        st.subheader('\n\nYou will sound incredibly professional with this email!\n')
         st.markdown(email_text)  #output the results
 
 
