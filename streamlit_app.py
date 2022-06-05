f978af55b3da51ada97133c8f982967ea4a70756
Stefan Rummer <82606558+stefanrmmr@users.noreply.github.com>
2022-06-05 05:37:54 +0200

    change output of mail

--- streamlit_app.py
+++ streamlit_app.py
@@ -100,7 +100,7 @@ def main_gpt3emailgen():
         with st.spinner():
             email_text = gen_mail_format(input_sender, input_recipient, input_contents)
 
-            st.text(email_text)  #output the results
+            st.code(email_text)  #output the results
 
 
 if __name__ == '__main__':
