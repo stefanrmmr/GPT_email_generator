f7ca17b7c8b9176af63cf897d18fe25373f0f13b
Stefan Rummer <82606558+stefanrmmr@users.noreply.github.com>
2022-06-06 15:53:00 +0200

    fix disappearing second topics

--- streamlit_app.py
+++ streamlit_app.py
@@ -69,7 +69,7 @@ def gen_mail_format(sender, recipient, email_contents):
         # prompt=f"Write a professional sounding email text that includes all of the following contents separately.\nThe text needs to be written to adhere to the specified writing styles and abbreviations need to be replaced.\n\nSender: {sender}\nRecipient: {recipient} {contents_str}\nWriting Styles: motivated, formal\n\nEmail Text:",
         temperature=0.8,
         max_tokens=contents_length*4,
-        top_p=0.4,
+        top_p=0,
         best_of=3,
         frequency_penalty=0,
         presence_penalty=1)
